ba02a01fc951ec802f7db74cc16e4dcb064b1507
youngjaekwon <gczzga@gmail.com>
2023-05-12 22:45:13 +0900

    feat(board) -- Post update receiver

---
+++ board/receivers.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Post
+
+
+@receiver(post_save, sender=Post)
+def handle_post_save(sendef, instance, **kwargs):
+    """
+    Post가 업데이트 될 때 Token 및 연관게시글을 업데이트 해야하는 상태로 변경
+    """
+    instance.update_needed = True
+    instance.save()
