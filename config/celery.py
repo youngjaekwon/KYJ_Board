0fcafd53e13896b81f0f9cc77636a3e6ffa82a8b
youngjaekwon <gczzga@gmail.com>
2023-05-13 17:19:19 +0900

    feat(board) -- create update post schedule every 5 minutes

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 
 import os
+from datetime import timedelta
 
 from celery import Celery
 
@@ -10,3 +11,14 @@ app = Celery("config")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    # Post의 토큰 및 연관게시글을 업데이트하는 schedule
+    "update_posts_every_five_minutes": {
+        "task": "board.tasks.update_post_task",
+        # 매 5분마다 실행
+        "schedule": timedelta(minutes=5),
+        # 실행 요청 후 2분이 지나도 실행이 안되면 expire
+        "options": {"expires": 2 * 60},  # 2minute
+    }
+}
